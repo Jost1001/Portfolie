7efe959877b21d8e097f7f60a446ff092961882e
Jost1001 <jost1001@stud.ek.dk>
2026-06-05 16:18:50 +0200

    Refactor accordion event listener; improve comments for better code clarity

--- js/accordion.js
+++ js/accordion.js
@@ -4,6 +4,9 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
+    // ^ toggle der tilføjer og fjerne klassen active
+
+    // v toggle der gemmer eller viser det aktive panel
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
